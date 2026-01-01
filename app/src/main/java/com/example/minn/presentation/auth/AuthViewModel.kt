d5a93b69ee76b0b575f535730f32b55b8c07288a
YaBuffy <aleksey-t@outlook.com>
2026-01-01 17:20:51 +0300

    add viewmodel

--- app/src/main/java/com/example/minn/presentation/auth/AuthViewModel.kt
+++ app/src/main/java/com/example/minn/presentation/auth/AuthViewModel.kt
@@ -1,12 +1,78 @@
 package com.example.minn.presentation.auth
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.minn.Util.Response
 import com.example.minn.domain.usecase.AuthUseCases
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
 class AuthViewModel @Inject constructor(
     private val authUseCases: AuthUseCases
 ): ViewModel() {
+
+    private val _state = MutableStateFlow((AuthUIState()))
+    val state = _state.asStateFlow()
+
+    init{
+        getAuthState()
+    }
+
+    private fun getAuthState(){
+        viewModelScope.launch {
+            authUseCases.getAuthState().collect {isAuth->
+                _state.value = _state.value.copy(isAuthorized = isAuth)
+
+            }
+        }
+
+    }
+
+    fun signUp(email: String, password: String){
+        viewModelScope.launch {
+            authUseCases.signUp(email, password).collect { response ->
+                handleResponse(response)
+            }
+        }
+    }
+    fun signIn(email: String, password: String){
+        viewModelScope.launch {
+            authUseCases.signIn(email, password).collect { response ->
+                handleResponse(response)
+            }
+        }
+    }
+    fun signOut(){
+        viewModelScope.launch {
+            authUseCases.signOut().collect { response ->
+                handleResponse(response)
+            }
+        }
+    }
+
+    private fun handleResponse(response: Response<Boolean>){
+        when (response){
+            is Response.Loading->{
+                _state.value = AuthUIState(isLoading = true)
+            }
+            is Response.Success->{
+                _state.value = AuthUIState(isAuthorized = response.data)
+            }
+            is Response.Error->{
+                _state.value = AuthUIState(error = response.message)
+            }
+        }
+    }
+
 }
+
+
+data class AuthUIState(
+    val isLoading: Boolean = false,
+    val isAuthorized: Boolean = false,
+    val error: String? = null
+)
